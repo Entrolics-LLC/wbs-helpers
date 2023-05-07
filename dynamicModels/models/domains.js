c1465cb16a2583c43ebb27872b3e9d584eb1fd51
syedfakher27 <sfakher27@gmail.com>
2023-05-07 23:00:49 +0500

    domain added in dynamic schema

---
+++ dynamicModels/models/domains.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+    Model, NOW
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Domain extends Model {
+        static associate(models) {
+            // define association here
+        }
+    }
+    Domain.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            domain: DataTypes.STRING,
+            description: DataTypes.STRING(1000),
+            parent_domain: DataTypes.STRING,
+            created_by: DataTypes.STRING,
+            is_deleted: DataTypes.BOOLEAN,
+            created_at: {
+                type: DataTypes.DATE
+            }
+        },
+        {
+            sequelize,
+            modelName: `${schema}_users`,
+            tableName: 'domains',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return Domain;
+};
