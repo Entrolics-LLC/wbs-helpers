b986e611992ba990c5925175d911ccf7ee2eeab8
syedfakher27 <sfakher27@gmail.com>
2023-05-31 01:45:59 +0500

    add search_history

---
+++ dynamicModels/models/search_history.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class SearchHistory extends Model {
+        static associate(models) {
+            // define association here
+        }
+    }
+    SearchHistory.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            user_id: DataTypes.STRING,
+            session_id: DataTypes.STRING,
+            question: DataTypes.STRING(1000),
+            result: DataTypes.JSON,
+            is_deleted: DataTypes.BOOLEAN,
+            created_at: {
+                type: DataTypes.DATE
+            }
+        },
+        {
+            sequelize,
+            modelName: `${schema}_search_historty`,
+            tableName: 'search_historty',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return SearchHistory;
+};
