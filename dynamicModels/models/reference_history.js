225f98b81e6f3779c902452adab749430d0494cb
myasir-khan <yasir.khan@aretecinc.com>
2023-07-04 21:51:40 +0500

    Update reference_history.js

--- dynamicModels/models/reference_history.js
+++ dynamicModels/models/reference_history.js
@@ -18,7 +18,8 @@ module.exports = (sequelize, DataTypes, schema) => {
             search: DataTypes.STRING(2000),
             reference_text: DataTypes.STRING(10000),
             file_name: DataTypes.STRING(512),
-            file_id: DataTypes.STRING,
+            domain: DataTypes.ARRAY(DataTypes.STRING),
+            uploaded_date: { type: DataTypes.DATE },
             es_confidence: DataTypes.FLOAT,
             is_deleted: DataTypes.BOOLEAN,
             created_at: {
