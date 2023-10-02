e96a8e470e111ca415adeb449c125697e4cea490
Haniya Maqsood <63501992+Hani1-2@users.noreply.github.com>
2023-10-02 18:38:24 +0500

    add prompt model

---
+++ dynamicModels/models/prompts.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class Prompt extends Model {
+        static associate(models) {
+            // define association here
+        }
+    }
+    Prompt.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            prompt: DataTypes.STRING,
+            description: DataTypes.STRING(1000),
+        },
+        {
+            sequelize,
+            modelName: `${schema}_prompts`,
+            tableName: 'prompts',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return Prompt;
+};
