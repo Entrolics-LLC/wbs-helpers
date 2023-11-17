317e64768399b05007dd589ec32e94202d447cd2
supremedev1987 <muhamadzeeshanahmed@gmail.com>
2023-11-17 15:48:06 +0500

    feat/diSearch matrix table

---
+++ dynamicModels/models/diSearch_matrices_questions.js
@@ -0,0 +1,48 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class DiSearch_Matrices_questions extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    DiSearch_Matrices_questions.init({
+        question_id: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false
+        },
+        question: DataTypes.STRING,
+        answer_type: DataTypes.STRING,
+        prompt: DataTypes.STRING(100000),
+        domains: DataTypes.ARRAY(DataTypes.STRING),
+        file_ids: DataTypes.ARRAY(DataTypes.STRING),
+        project_id: DataTypes.STRING,
+        is_deleted: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        created_at: {
+            type: DataTypes.DATE
+        },
+        updated_at: {
+            type: DataTypes.DATE
+        },
+    },
+        {
+            sequelize,
+            modelName: `${schema}_diSearch_matrices_questions`,
+            tableName: 'diSearch_matrices_questions',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return DiSearch_Matrices_questions;
+};
---
+++ dynamicModels/models/diSearch_matrices_requirements.js
@@ -0,0 +1,45 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class DiSearch_Matrices_requirements extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    DiSearch_Matrices_requirements.init({
+        requirement_id: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false
+        },
+        project_id: DataTypes.STRING,
+        section_value: DataTypes.STRING,
+        requirement_value: DataTypes.STRING(100000),
+        is_deleted: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        created_at: {
+            type: DataTypes.DATE
+        },
+        updated_at: {
+            type: DataTypes.DATE
+        },
+    },
+        {
+            sequelize,
+            modelName: `${schema}_diSearch_matrices_requirements`,
+            tableName: 'diSearch_matrices_requirements',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        })
+    return DiSearch_Matrices_requirements
+};
