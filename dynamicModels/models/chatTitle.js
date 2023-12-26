6f41cd672617479f6ffb881dc48ac750bb924122
Muhammad Areeb <79369873+Areeb-dev@users.noreply.github.com>
2023-12-27 02:13:10 +0500

    Added Chat Title Table

---
+++ dynamicModels/models/chatTitle.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class chatTitle extends Model {
+        static associate(models) {
+            // define association here
+        }
+    }
+    chatTitle.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            user_id: DataTypes.STRING,
+            session_id: DataTypes.STRING,
+            title: DataTypes.STRING,
+            is_deleted: DataTypes.BOOLEAN,
+            updated_at: {
+                type: DataTypes.DATE
+            },
+            created_at: {
+                type: DataTypes.DATE
+            }
+        },
+        {
+            sequelize,
+            modelName: `${schema}_chat_title`,
+            tableName: 'chat_title',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return chatTitle;
+};
