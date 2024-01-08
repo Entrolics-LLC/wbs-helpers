8fc3ebd850d9857e73049aa14257335692c9ddf6
Mansoor Hussain <hussainmansoor876@gmail.com>
2024-01-09 00:52:01 +0500

    Create users_queries.js

---
+++ dynamicModels/models/users_queries.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class UserQueries extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    UserQueries.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            user_id: DataTypes.STRING,
+            query: DataTypes.STRING,
+            query_type: DataTypes.STRING,
+            created_at: {
+                type: DataTypes.DATE
+            },
+            updated_at: {
+                type: DataTypes.DATE
+            }
+        },
+        {
+            sequelize,
+            modelName: `${schema}_users_queries`,
+            tableName: 'users',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return UserQueries;
+};
