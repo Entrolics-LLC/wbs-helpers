aabf8a62f2980244f2c407d4e2871a7bc68f2b90
Syed Fakher <sfakher27@gmail.com>
2024-01-24 03:19:11 +0500

    Create pensdown_prompts.js

---
+++ dynamicModels/models/pensdown_prompts.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class PensdownPrompt extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    PensdownPrompt.init({
+        prompt_id: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false
+        },
+        prompt_name: DataTypes.STRING,
+        prompt_page: DataTypes.STRING,
+        prompt_value: DataTypes.STRING,
+        is_deleted: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        updated_at: {
+            type: DataTypes.DATE
+        },
+    },
+        {
+            sequelize,
+            modelName: `${schema}_pensdown_prompts`,
+            tableName: 'pensdown_prompts',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return PensdownPrompt;
+};
