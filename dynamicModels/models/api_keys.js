16db06ac70dca340683e7df0c5120269dee20adb
Mansoor Hussain <hussainmansoor876@gmail.com>
2024-02-27 16:42:11 +0500

    Create api_keys.js

---
+++ dynamicModels/models/api_keys.js
@@ -0,0 +1,50 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class APIKey extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    APIKey.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            name: DataTypes.STRING,
+            is_deleted: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            },
+            is_validated: {
+                type: DataTypes.BOOLEAN
+            },
+            created_at: {
+                type: DataTypes.DATE
+            },
+            updated_at: {
+                type: DataTypes.DATE
+            },
+            expire_at: {
+                type: DataTypes.DATE
+            },
+        },
+        {
+            sequelize,
+            modelName: `${schema}_api_key`,
+            tableName: 'api_key',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return APIKey;
+};
