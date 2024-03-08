1e0930e40db79715a1a1508ea276b9583e3134c9
Muhammad Areeb <79369873+Areeb-dev@users.noreply.github.com>
2024-03-08 17:59:14 +0500

    Update documents.js

--- dynamicModels/models/documents.js
+++ dynamicModels/models/documents.js
@@ -28,6 +28,7 @@ module.exports = (sequelize, DataTypes, schema) => {
             parser: DataTypes.STRING,
             dag_run_id: DataTypes.STRING,
             proposal_id: DataTypes.STRING,
+            chat_custom_inst_id: DataTypes.STRING,
             error: DataTypes.STRING(1098765),
             status: DataTypes.ARRAY(DataTypes.STRING),
             domain: DataTypes.ARRAY(DataTypes.STRING),
