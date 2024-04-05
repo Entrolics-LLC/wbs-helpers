d22c0e0e21f2987b9e594e5b6b0c10bf72cde713
Muhammad Areeb <79369873+Areeb-dev@users.noreply.github.com>
2024-04-05 23:34:31 +0500

    Update custom_instructions.js

--- dynamicModels/models/custom_instructions.js
+++ dynamicModels/models/custom_instructions.js
@@ -21,6 +21,7 @@ module.exports = (sequelize, DataTypes, schema) => {
             end_prompt: DataTypes.STRING(1098765),
             tone: DataTypes.ARRAY(DataTypes.STRING),
             shared_with: DataTypes.ARRAY(DataTypes.STRING),
+            llmModel: DataTypes.JSONB,
             is_section: {
                 type: DataTypes.BOOLEAN,
                 defaultValue: false
