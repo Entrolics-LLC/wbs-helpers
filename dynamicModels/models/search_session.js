5e08af2f04f4b5623acfdade52f5ddf78ea9d830
Muhammad Areeb <79369873+Areeb-dev@users.noreply.github.com>
2024-04-07 00:26:14 +0500

    Fixed search session table

--- dynamicModels/models/search_session.js
+++ dynamicModels/models/search_session.js
@@ -2,40 +2,39 @@
 const {
     Model
 } = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+module.exports = (sequelize, DataTypes, schema) => {
     class searchSession extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
         static associate(models) {
             // define association here
         }
     }
-    searchSession.init({
-        id: {
-            type: DataTypes.STRING,
-            primaryKey: true
+    searchSession.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true
+            },
+            user_id: DataTypes.STRING,
+            is_deleted: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            },
+            updated_at: {
+                type: DataTypes.DATE,
+                allowNull: false
+            },
+            created_at: {
+                type: DataTypes.DATE,
+                allowNull: false
+            }
         },
-        user_id: DataTypes.STRING,
-        is_deleted: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        },
-        updated_at: {
-            type: DataTypes.DATE,
-            allowNull: false
-        },
-        created_at: {
-            type: DataTypes.DATE,
-            allowNull: false
-        }
-    }, {
-        sequelize,
-        modelName: 'search_session',
-        createdAt: false,
-        updatedAt: false
-    });
+        {
+            sequelize,
+            modelName: `${schema}_search_session`,
+            tableName: 'search_session',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
     return searchSession;
 };
