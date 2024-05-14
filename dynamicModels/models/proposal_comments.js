94cbcd92ad9fcf298aea28bdb97f64f79aa0a3ca
supremedev1987 <95992858+MuhammadZeeshanAhmed@users.noreply.github.com>
2024-05-14 18:56:20 +0500

    added proposal-sections-highlight table

---
+++ dynamicModels/models/proposal_comments.js
@@ -0,0 +1,51 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class ProposalSectionsHighlight extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    ProposalSectionsHighlight.init({
+        id: {
+            type: DataTypes.STRING,
+            primaryKey: true,
+            allowNull: false
+        },
+        user_id: DataTypes.STRING,
+        proposal_id: DataTypes.STRING,
+        file_id: DataTypes.STRING,
+        section_c: DataTypes.STRING,
+        section_l: DataTypes.STRING,
+        section_m: DataTypes.STRING,
+        section_c_position: DataTypes.JSONB,
+        section_l_position: DataTypes.JSONB,
+        section_m_position: DataTypes.JSONB,
+        is_deleted: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        created_at: {
+            type: DataTypes.DATE
+        },
+        updated_at: {
+            type: DataTypes.DATE
+        }
+    },
+        {
+            sequelize,
+            modelName: `${schema}_proposal_sections_higlight`,
+            tableName: 'proposal_sections_higlight',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return ProposalSectionsHighlight;
+};
