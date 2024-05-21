042e8a462651c8c54ff585cbef33545b23832977
supremedev1987 <95992858+MuhammadZeeshanAhmed@users.noreply.github.com>
2024-05-21 19:06:34 +0500

    updated

--- dynamicModels/models/proposal_search_session.js
+++ dynamicModels/models/proposal_search_session.js
@@ -16,7 +16,6 @@ module.exports = (sequelize, DataTypes, schema) => {
             },
             user_id: DataTypes.STRING,
             title: DataTypes.STRING(1098765),
-            file_ids: DataTypes.ARRAY(DataTypes.STRING),
             domains: DataTypes.ARRAY(DataTypes.STRING),
             di_search: {
                 type: DataTypes.BOOLEAN,
