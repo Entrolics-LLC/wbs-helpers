5d08c79a260a29db3eb7f0d74241c50617c775f8
supremedev1987 <95992858+MuhammadZeeshanAhmed@users.noreply.github.com>
2024-05-21 17:49:44 +0500

    feat/ proposal chat tables

---
+++ dynamicModels/models/proposal_search_history.js
@@ -0,0 +1,44 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class ProposalSearchHistory extends Model {
+        static associate(models) {
+            // define association here
+        }
+    }
+    ProposalSearchHistory.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            user_id: DataTypes.STRING,
+            session_id: DataTypes.STRING,
+            question: DataTypes.STRING(1098765),
+            answer: DataTypes.STRING(1098765),
+            domains: DataTypes.ARRAY(DataTypes.STRING),
+            is_deleted: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            },
+            created_at: {
+                type: DataTypes.DATE
+            },
+            updated_at: {
+                type: DataTypes.DATE,
+                allowNull: true
+            }
+        },
+        {
+            sequelize,
+            modelName: `${schema}_proposal_search_history`,
+            tableName: 'proposal_search_history',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return ProposalSearchHistory;
+};
---
+++ dynamicModels/models/proposal_search_session.js
@@ -0,0 +1,46 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class ProposalSearchSession extends Model {
+        static associate(models) {
+            // define association here
+        }
+    }
+    ProposalSearchSession.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true
+            },
+            user_id: DataTypes.STRING,
+            title: DataTypes.STRING(1098765),
+            file_ids: DataTypes.ARRAY(DataTypes.STRING),
+            di_search: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            },
+            is_deleted: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            },
+            updated_at: {
+                type: DataTypes.DATE,
+                allowNull: true
+            },
+            created_at: {
+                type: DataTypes.DATE,
+                allowNull: false
+            }
+        },
+        {
+            sequelize,
+            modelName: `${schema}_proposal_search_session`,
+            tableName: 'proposal_search_session',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return ProposalSearchSession;
+};
