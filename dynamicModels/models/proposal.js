78c6df8f796d58ca42d5b579c42779bccc2c5dfa
myasir-khan <yasir.khan@aretecinc.com>
2024-06-05 18:33:39 +0500

    Update proposal.js

--- dynamicModels/models/proposal.js
+++ dynamicModels/models/proposal.js
@@ -27,7 +27,7 @@ module.exports = (sequelize, DataTypes, schema) => {
             proposal_manager: DataTypes.ARRAY(DataTypes.STRING),
             file_url: DataTypes.STRING,
             gs_urls: DataTypes.ARRAY(DataTypes.STRING),
-            background: DataTypes.ARRAY(DataTypes.STRING),
+            background: DataTypes.STRING(10485760),
             api_key: DataTypes.STRING,
             status: DataTypes.STRING,
             proposal_type: DataTypes.STRING,
