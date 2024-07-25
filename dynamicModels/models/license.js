e8f92b3a4b2aa92f2e13d32d9f10fc99d830570c
Muhammad Areeb <79369873+Areeb-dev@users.noreply.github.com>
2024-07-26 00:08:26 +0500

    added license table

---
+++ dynamicModels/models/license.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class License extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    License.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            procurement_account_id: DataTypes.STRING,
+            entitlement_id: DataTypes.STRING,
+            licenses: DataTypes.INTEGER,
+            updated_at: {
+                type: DataTypes.DATE
+            },
+            created_at: {
+                type: DataTypes.DATE
+            }
+        },
+        {
+            sequelize,
+            modelName: `${schema}_license`,
+            tableName: 'license',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return License;
+};
