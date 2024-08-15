7737e0886b2446e4d80220267c11d7edd670355f
Muhammad Areeb <79369873+Areeb-dev@users.noreply.github.com>
2024-08-15 18:17:30 +0500

    86b1prgz4 : added pricing tables

---
+++ dynamicModels/models/plan_subscriber.js
@@ -0,0 +1,67 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class PlanSubscriber extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    PlanSubscriber.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            user_id: DataTypes.STRING,
+            subscription_id: DataTypes.STRING,
+            customer_id: DataTypes.STRING,
+            product_id: DataTypes.STRING,
+            plan_name: DataTypes.STRING,
+            no_of_guest: DataTypes.FLOAT,
+            no_of_license: DataTypes.FLOAT,
+            no_of_search: DataTypes.FLOAT,
+            no_of_used_search: {
+                type: DataTypes.FLOAT,
+                defaultValue: 0
+            },
+            plan_type: DataTypes.STRING,
+            plan_amount: DataTypes.FLOAT,
+            plan_description: DataTypes.STRING,
+            plan_credits: DataTypes.FLOAT,
+            ended_date: DataTypes.STRING,
+            is_cancelled: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            },
+            is_deleted: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            },
+            expiry_at: {
+                type: DataTypes.DATE
+            },
+            created_at: {
+                type: DataTypes.DATE
+            },
+            updated_at: {
+                type: DataTypes.DATE
+            }
+        },
+        {
+            sequelize,
+            modelName: `${schema}_plan_subscriber`,
+            tableName: 'plan_subscriber',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return PlanSubscriber;
+};
