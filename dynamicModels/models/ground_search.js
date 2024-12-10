a48a7ea90ab05525564772b833f8eb852e75a112
myasir-khan <137196859+myasir-khan@users.noreply.github.com>
2024-12-10 19:40:06 +0500

    Create ground_search.js

---
+++ dynamicModels/models/ground_search.js
@@ -0,0 +1,40 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class GroundSearch extends Model {
+        static associate(models) {
+            // define association here
+        }
+    }
+    GroundSearch.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            user_id: DataTypes.STRING(1098765),
+            search_id: DataTypes.STRING(1098765),
+            question: DataTypes.STRING(1098765),
+            response: DataTypes.STRING(1098765),
+            sources: DataTypes.JSONB,
+            is_deleted: DataTypes.BOOLEAN,
+            created_at: {
+                type: DataTypes.DATE
+            },
+            updated_at: {
+                type: DataTypes.DATE
+            }
+        },
+        {
+            sequelize,
+            modelName: `${schema}_ground_search`,
+            tableName: 'ground_search',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        })
+    return GroundSearch;
+};
