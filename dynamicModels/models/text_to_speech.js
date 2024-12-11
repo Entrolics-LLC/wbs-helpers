186b55b9615a904face1f7ff5ed04a5965339b5c
Muhammad Areeb <muhammad.areeb@aretec.ai>
2024-12-11 18:23:34 +0500

    Added TextToSpeech table

---
+++ dynamicModels/models/text_to_speech.js
@@ -0,0 +1,50 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class TextToSpeech extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    TextToSpeech.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            user_id: DataTypes.STRING,
+            session_id: DataTypes.STRING,
+            type: DataTypes.STRING,
+            customization: DataTypes.JSONB,
+            session_name: DataTypes.STRING(1000),
+            audio_file_url: DataTypes.STRING,
+            status: DataTypes.STRING,
+            is_deleted: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            },
+            created_at: {
+                type: DataTypes.DATE
+            },
+            updated_at: {
+                type: DataTypes.DATE
+            }
+        },
+        {
+            sequelize,
+            modelName: `${schema}_texttospeech`,
+            tableName: 'texttospeech',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return TextToSpeech;
+};
