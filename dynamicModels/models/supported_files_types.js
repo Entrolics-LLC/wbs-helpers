1fec1d2009446aa9e8b05d7548a8b49479518062
myasir-khan <137196859+myasir-khan@users.noreply.github.com>
2024-12-13 18:54:48 +0500

    Create supported_files_types.js

---
+++ dynamicModels/models/supported_files_types.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class SupportedFileTypes extends Model {
+        static associate(models) {
+            // define association here
+        }
+    }
+    SupportedFileTypes.init(
+        {
+            s_no: DataTypes.INTEGER,
+            type: DataTypes.STRING,
+            size: DataTypes.INTEGER,
+            is_deleted: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            }
+        },
+        {
+            sequelize,
+            modelName: `${schema}_supported_files_types`,
+            tableName: 'supported_files_types',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return SupportedFileTypes;
+};
