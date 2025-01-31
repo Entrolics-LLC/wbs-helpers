d6a7ca06bc66d83d0d809e3ed9c04a4a6a203273
Muhammad Areeb <muhammad.areeb@aretec.ai>
2025-01-31 22:05:36 +0500

    Update vertexai_settings.js

--- dynamicModels/models/vertexai_settings.js
+++ dynamicModels/models/vertexai_settings.js
@@ -17,7 +17,10 @@ module.exports = (sequelize, DataTypes, schema) => {
             },
             relevance: DataTypes.FLOAT,
             recency_boost: DataTypes.FLOAT,
-            intent_detection: DataTypes.BOOLEAN,
+            intent_detection: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            },
             recency_boost_enable: DataTypes.BOOLEAN,
             entity_person_boost: DataTypes.FLOAT,
             entity_person_boost_enable: DataTypes.BOOLEAN,
