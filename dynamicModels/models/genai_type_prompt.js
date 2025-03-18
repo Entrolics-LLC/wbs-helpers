d35119eaf04fb1f87931cf64e0349c4f37793259
Muhammad Areeb <muhammad.areeb@aretec.ai>
2025-03-19 00:29:42 +0500

    Added genai_type_prompt

---
+++ dynamicModels/models/genai_type_prompt.js
@@ -0,0 +1,43 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class GenaiTypePrompt extends Model {
+        static associate(models) {
+            // define association here
+        }
+    }
+    GenaiTypePrompt.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            genai_type_id: DataTypes.STRING,
+            model_id: DataTypes.STRING,
+            name: DataTypes.STRING,
+            prompt: DataTypes.STRING(999999),
+            is_default: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            },
+            is_deleted: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            },
+            created_at: {
+                type: DataTypes.DATE
+            }
+        },
+        {
+            sequelize,
+            modelName: `${schema}_genai_type_prompt`,
+            tableName: 'genai_type_prompt',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return GenaiTypePrompt;
+};
