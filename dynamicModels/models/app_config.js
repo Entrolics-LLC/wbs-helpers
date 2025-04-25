96443ba0c868473db1d8aca01c4a3c1f2abdccd7
supremedev1987 <95992858+MuhammadZeeshanAhmed@users.noreply.github.com>
2025-04-25 18:09:06 +0500

    Create app_config.js

---
+++ dynamicModels/models/app_config.js
@@ -0,0 +1,42 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class AppConfig extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    AppConfig.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            title: DataTypes.STRING(255),
+            description: DataTypes.STRING(10485760),
+            logo: DataTypes.STRING(255),
+            created_at: {
+                type: DataTypes.DATE
+            },
+            updated_at: {
+                type: DataTypes.DATE
+            }
+        },
+        {
+            sequelize,
+            modelName: `${schema}_app_config`,
+            tableName: 'app_config',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return AppConfig;
+};
