d0c2771579c55a0ed97b1f5d2851494331708b16
myasir-khan <137196859+myasir-khan@users.noreply.github.com>
2025-10-16 22:46:02 +0500

    Add ShareSession Sequelize model
    Introduces the ShareSession model for managing share session data, including fields for id, user_id, url, and created_at. The model is initialized with custom schema and table name support, and disables automatic timestamps.

---
+++ dynamicModels/models/share_session.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class ShareSession extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    ShareSession.init(
+        {
+            id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            user_id: DataTypes.STRING(255),
+            url: DataTypes.STRING(1048796),
+            created_at: {
+                type: DataTypes.DATE
+            }
+        },
+        {
+            sequelize,
+            modelName: `${schema}_share_session`,
+            tableName: 'share_session',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        })
+    return ShareSession;
+}
