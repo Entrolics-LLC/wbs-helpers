567841a98c08018ef97961de41a51714b4ccf138
myasir-khan <137196859+myasir-khan@users.noreply.github.com>
2025-11-26 21:13:01 +0500

    Create group.js

---
+++ dynamicModels/models/group.js
@@ -0,0 +1,45 @@
+'use strict';
+const {
+    Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes, schema) => {
+    class Group extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Group.init(
+        {
+            group_id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                allowNull: false
+            },
+            group_name: DataTypes.STRING,
+            user_id: DataTypes.STRING,
+            created_at: {
+                type: DataTypes.DATE
+            },
+            updated_at: {
+                type: DataTypes.DATE
+            },
+            is_deleted: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            },
+        },
+        {
+            sequelize,
+            modelName: `${schema}_group`,
+            tableName: 'group',
+            schema,
+            createdAt: false,
+            updatedAt: false
+        });
+    return Group;
+};
