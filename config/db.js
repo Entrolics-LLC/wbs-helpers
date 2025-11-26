beea65f10f23f6fa3f1a9dbfb78a98c8577bb710
myasir-khan <137196859+myasir-khan@users.noreply.github.com>
2025-11-26 22:26:10 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -89,16 +89,6 @@ const initPromise = (cloudConfig = config, schema, alter = false) => {
     }
 }
 
-// const createDB = () => {
-//     exec('cd node_modules && cd context-helpers && npm run createDB', (error, stdout, stderr) => {
-//         console.log('stdout: ' + stdout)
-//         console.log('stderr: ' + stderr)
-//         if (error !== null) {
-//             console.log('exec error: ' + error)
-//         }
-//     })
-// }
-
 const migrateDB = () => {
     try {
         let migration = process.env.NODE_ENV === 'production' ? 'createMigrationProduction' : 'createMigration'
@@ -114,18 +104,6 @@ const migrateDB = () => {
         console.log('***')
     }
 }
-
-// var sequelize = new Sequelize(config.database, config.username, config.password, {
-//     host: config.host,
-//     port: 5432,
-//     maxConcurrentQueries: 100,
-//     dialect: 'postgres',
-//     dialectOptions: {
-//         ssl: true
-//     },
-//     pool: { maxConnections: 5, maxIdleTime: 30 },
-//     language: 'en'
-// })
 
 module.exports = {
     init,
